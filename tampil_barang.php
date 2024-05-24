22850961e199d95481d34cdc75165c6857b44f68
raldybn <raldybelo@gmail.com>
2024-05-24 16:04:22 +0700

    pembaruan

--- tampil_barang.php
+++ tampil_barang.php
@@ -37,6 +37,7 @@ if(mysqli_num_rows($q)) {
     echo 'barang ada sebanyak '.$jumlah_barang;
 
     //looping dgn while
+    $divs = '';
     while($d = mysqli_fetch_assoc($q)){
 
          //tekink debugging
@@ -46,32 +47,36 @@ if(mysqli_num_rows($q)) {
         // exit;
         // echo '<br>barang zzz';
 
-    // tampilkan form delete jika tidak ada order
-    $form_delete = $d['jumlah_order'] ? "jumlah Order: $d[jumlah_order]" : "
-    <form method=post>
-     <button name=btn_delete value=$d[kode_barang]>DELETE
-     </button>
-    </form>
-    ";
-
-
-    $btn_edit = $username ? "<button>EDIT</button>" : '';
-    $btn_delete = $username ? $form_delete : '';
-
-
-        echo "
-        <div class=card>
-            <div>$d[nama_barang]</div>
-            <img src='img/$d[gambar]' class='gambar'>
-            <div>$d[harga_jual]</div>
-            <a href='order.php?kode_barang=$d[kode_barang]'>
-                <button>BELI</button>
-            </a>
-            $btn_edit
-            $btn_delete
-            <br>
-        </div>
+        // tampilkan form delete jika tidak ada order
+        $form_delete = $d['jumlah_order'] ? "jumlah Order: $d[jumlah_order]" : "
+        <form method=post>
+        <button name=btn_delete value=$d[kode_barang]>DELETE
+        </button>
+        </form>
         ";
+
+
+        $btn_edit = $username ? "<a href='?kode_barang=$d[kode_barang]'<button>EDIT</button>" : '';
+        $btn_delete = $username ? $form_delete : '';
+
+            //tampil sebagai card
+
+            $divs .= "
+            <div class=card>
+                <div>$d[nama_barang]</div>
+                <img src='img/$d[gambar]' class='gambar'>
+                <div>$d[harga_jual]</div>
+                <a href='order.php?kode_barang=$d[kode_barang]'>
+                    <button>BELI</button>
+                </a>
+                $btn_edit
+                $btn_delete
+            </div>
+            ";
+
+    }
+
+    echo "<div style='display: flex;'>$divs</div>";
 
         // tambah barang
         echo !$username ? '' : "
@@ -88,7 +93,6 @@ if(mysqli_num_rows($q)) {
         ";
 
 
-    }
-}else{
+    }else{
     echo 'barang kosong';
 }
